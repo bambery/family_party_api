56602143708cc8e21a33265b343555f7240ecd6d
bambery <lwszolek@gmail.com>
2024-02-04 18:13:28 -0600

    Adding basic request tests for User

--- Gemfile
+++ Gemfile
@@ -39,10 +39,13 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
   gem "rspec-rails", "~>6.0.0"
+  gem "factory_bot_rails", "~> 6.4"
+  gem "faker", "~> 3.2"
 end
 
 group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
+
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
   validates :first_name,
     presence: true,
     length: { minimum: 3, maximum: 25 },
-    format: { with: /\A[a-zA-Z']+\s?[a-zA-Z]+\z/, message: "only letters, apostrophes, and single blank spaces are allowed in names" }
+    format: { with: /\A[a-zA-Z']+\s?[a-zA-Z]+\z/, message: "only letters, apostrophes, and single blank spaces are allowed in first names" }
   validates :last_name,
     presence: true,
     length: { minimum: 3, maximum: 25 },
-    format: { with: /\A[a-zA-Z']+\s?[a-zA-Z]+\z/, message: "only letters, apostrophes, and single blank spaces are allowed in names" }
+    format: { with: /\A[a-zA-Z'\-]+[\s\-]?[a-zA-Z]+\z/, message: "only letters, apostrophes, and a single blank space or a hyphen are allowed in last names" }
 
 end
